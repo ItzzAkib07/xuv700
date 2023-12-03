7ea58338cf554d7679c3b7f002f57c354f5d88c6
Akib Mulla <akibmulla47@gmail.com>
2023-12-03 20:34:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  var blist = document.querySelector(".blist");
+  var scrollLeftButton = document.querySelector(".scroll-left");
+  var scrollRightButton = document.querySelector(".scroll-right");
+
+  scrollLeftButton.addEventListener("click", function () {
+    blist.scrollTo({
+      left: blist.scrollLeft - 500,
+      behavior: 'smooth'
+    });
+  });
+
+  scrollRightButton.addEventListener("click", function () {
+    blist.scrollTo({
+      left: blist.scrollLeft + 500,
+      behavior: 'smooth'
+    });
+  });
+};
